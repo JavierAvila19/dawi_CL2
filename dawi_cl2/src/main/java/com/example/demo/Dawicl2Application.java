bb593cf2926cbaa7551b497d2a46f69578ac3348
Javier <javieravila2819@gmail.com>
2023-05-25 17:39:07 -0500

    creacion del proyecto y paquetes

---
+++ dawi_cl2/src/main/java/com/example/demo/Dawicl2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawicl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawicl2Application.class, args);
+	}
+
+}
